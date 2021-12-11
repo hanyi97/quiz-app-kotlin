365b1a50615a9713587200bf2ea4d9ec55c9c458
Ng Han Yi <nghanyi1997@gmail.com>
2021-12-11 23:06:28 +0800

    Implement binding so no need to use findbyid

--- app/src/main/java/com/nghanyi/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/nghanyi/quizapp/QuizQuestionActivity.kt
@@ -1,54 +1,74 @@
 package com.nghanyi.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.nghanyi.quizapp.databinding.ActivityMainBinding
+import com.nghanyi.quizapp.databinding.ActivityQuizQuestionBinding
 import org.w3c.dom.Text
 
 class QuizQuestionActivity : AppCompatActivity() {
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
+
+    private lateinit var binding: ActivityQuizQuestionBinding // So no need to use findViewById
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz_question)
+        binding = ActivityQuizQuestionBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
 
-        val questionsList = Constants.getQuestions()
-        Log.i("Questions Size", "${questionsList.size}")
+        mQuestionList = Constants.getQuestions()
+        mCurrentPosition = 1
 
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition-1]
-
-        showQuestion(question)
-        showProgress(currentPosition)
-
+        setQuestion()
+        showProgress()
     }
 
-    private fun showQuestion(question: Question?) {
-        if (question == null) return
+    /**
+     * Set current question to be shown
+     */
+    private fun setQuestion() {
+        val question = mQuestionList!![mCurrentPosition-1]
 
-        val ivImage = findViewById<ImageView>(R.id.iv_image)
+        defaultOptionsView()
 
-        val tvQuestion = findViewById<TextView>(R.id.tv_question)
-        val tvOptionOne = findViewById<TextView>(R.id.tv_option_one)
-        val tvOptionTwo = findViewById<TextView>(R.id.tv_option_two)
-        val tvOptionThree = findViewById<TextView>(R.id.tv_option_three)
-        val tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
-
-        ivImage.setImageResource(question.image)
-
-        tvQuestion.text = question.question
-        tvOptionOne.text = question.optionOne
-        tvOptionTwo.text = question.optionTwo
-        tvOptionThree.text = question.optionThree
-        tvOptionFour.text = question.optionFour
+        binding.ivImage.setImageResource(question.image)
+        binding.tvQuestion.text = question.question
+        binding.tvOptionOne.text = question.optionOne
+        binding.tvOptionTwo.text = question.optionTwo
+        binding.tvOptionThree.text = question.optionThree
+        binding.tvOptionFour.text = question.optionFour
     }
 
-    private fun showProgress(currentProgress: Int) {
-        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        val tvProgress = findViewById<TextView>(R.id.tv_progress)
+    /**
+     * Set current progress of user
+     */
+    private fun showProgress() {
+        binding.progressBar.progress = mCurrentPosition
+        binding.tvProgress.text = "$mCurrentPosition/${binding.progressBar.max}"
+    }
 
-        progressBar.progress = currentProgress
-        tvProgress.text = "$currentProgress/${progressBar.max}"
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
+        options.add(0, binding.tvOptionOne)
+        options.add(1, binding.tvOptionTwo)
+        options.add(2, binding.tvOptionThree)
+        options.add(3, binding.tvOptionFour)
+
+        for (option in options) {
+            option.typeface = Typeface.DEFAULT
+            option.setTextColor(ContextCompat.getColor(this, R.color.gray))
+            option.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
+        }
     }
 }
