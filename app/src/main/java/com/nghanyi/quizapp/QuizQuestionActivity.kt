e194c4e2942e4ed23774edbd6a356668fdc544d1
Ng Han Yi <nghanyi1997@gmail.com>
2021-12-11 23:15:40 +0800

    Set selected option

--- app/src/main/java/com/nghanyi/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/nghanyi/quizapp/QuizQuestionActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -14,7 +15,7 @@ import com.nghanyi.quizapp.databinding.ActivityMainBinding
 import com.nghanyi.quizapp.databinding.ActivityQuizQuestionBinding
 import org.w3c.dom.Text
 
-class QuizQuestionActivity : AppCompatActivity() {
+class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
 
     private var mCurrentPosition: Int = 1
     private var mQuestionList: ArrayList<Question>? = null
@@ -32,6 +33,12 @@ class QuizQuestionActivity : AppCompatActivity() {
 
         setQuestion()
         showProgress()
+
+        binding.tvOptionOne.setOnClickListener(this)
+        binding.tvOptionTwo.setOnClickListener(this)
+        binding.tvOptionThree.setOnClickListener(this)
+        binding.tvOptionFour.setOnClickListener(this)
+
     }
 
     /**
@@ -67,8 +74,25 @@ class QuizQuestionActivity : AppCompatActivity() {
 
         for (option in options) {
             option.typeface = Typeface.DEFAULT
-            option.setTextColor(ContextCompat.getColor(this, R.color.gray))
+            option.setTextColor(ContextCompat.getColor(this, R.color.lightGray))
             option.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
         }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.tv_option_one -> selectedOptionView(binding.tvOptionOne, 1)
+            R.id.tv_option_two -> selectedOptionView(binding.tvOptionTwo, 2)
+            R.id.tv_option_three -> selectedOptionView(binding.tvOptionThree, 3)
+            R.id.tv_option_four -> selectedOptionView(binding.tvOptionFour, 4)
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.setTextColor(ContextCompat.getColor(this, R.color.gray))
+        tv.background = ContextCompat.getDrawable(this, R.drawable.selected_option_border_bg)
     }
 }
