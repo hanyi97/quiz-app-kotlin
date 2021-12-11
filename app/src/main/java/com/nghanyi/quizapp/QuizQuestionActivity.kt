99e93357b9701656d50c2950150308630d9bec1e
Ng Han Yi <nghanyi1997@gmail.com>
2021-12-11 22:28:58 +0800

    Load questions to view

--- app/src/main/java/com/nghanyi/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/nghanyi/quizapp/QuizQuestionActivity.kt
@@ -3,6 +3,10 @@ package com.nghanyi.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class QuizQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +15,40 @@ class QuizQuestionActivity : AppCompatActivity() {
 
         val questionsList = Constants.getQuestions()
         Log.i("Questions Size", "${questionsList.size}")
+
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition-1]
+
+        showQuestion(question)
+        showProgress(currentPosition)
+
+    }
+
+    private fun showQuestion(question: Question?) {
+        if (question == null) return
+
+        val ivImage = findViewById<ImageView>(R.id.iv_image)
+
+        val tvQuestion = findViewById<TextView>(R.id.tv_question)
+        val tvOptionOne = findViewById<TextView>(R.id.tv_option_one)
+        val tvOptionTwo = findViewById<TextView>(R.id.tv_option_two)
+        val tvOptionThree = findViewById<TextView>(R.id.tv_option_three)
+        val tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
+
+        ivImage.setImageResource(question.image)
+
+        tvQuestion.text = question.question
+        tvOptionOne.text = question.optionOne
+        tvOptionTwo.text = question.optionTwo
+        tvOptionThree.text = question.optionThree
+        tvOptionFour.text = question.optionFour
+    }
+
+    private fun showProgress(currentProgress: Int) {
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val tvProgress = findViewById<TextView>(R.id.tv_progress)
+
+        progressBar.progress = currentProgress
+        tvProgress.text = "$currentProgress/${progressBar.max}"
     }
 }
