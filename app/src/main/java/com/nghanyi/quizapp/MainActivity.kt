2e33f31e9f56f06807931d120d2964d2838ea52d
Ng Han Yi <nghanyi1997@gmail.com>
2021-12-11 21:47:52 +0800

    Setup questions

--- app/src/main/java/com/nghanyi/quizapp/MainActivity.kt
+++ app/src/main/java/com/nghanyi/quizapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.nghanyi.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +15,17 @@ class MainActivity : AppCompatActivity() {
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         actionBar?.hide()
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val etName = findViewById<EditText>(R.id.et_name)
+        btnStart.setOnClickListener {
+            if (etName.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionActivity::class.java)
+                startActivity(intent)
+                finish() // Close current activity
+            }
+        }
     }
 }
